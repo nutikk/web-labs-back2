3d6b63202791bc2d33cab9e9c668ba4b7d7702c7
Анна Петерс <an_peters@mail.ru>
2024-09-18 20:00:09 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def not_found(err):
     return "Нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html> 
         <html>
@@ -19,7 +19,7 @@ def web():
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author(): 
     name = "Петерс Анна Алексеевна"
     group = "ФБИ-21"
@@ -102,7 +102,7 @@ def reset_counter():
 '''
 
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
     return redirect("/author")
 
