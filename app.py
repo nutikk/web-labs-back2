5b44a8116b73c6d50d76db37a3dc5eb2a1d9b942
Анна Петерс <an_peters@mail.ru>
2024-10-06 12:56:16 +0700

    Имя студента

--- app.py
+++ app.py
@@ -431,4 +431,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Анна Петерс'
+    return render_template('example.html', name=name)
