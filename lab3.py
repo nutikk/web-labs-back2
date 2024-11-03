1bc2fc3d04a879c696ef3e083ef8cec19d986cc6
Анна Петерс <an_peters@mail.ru>
2024-11-03 21:07:01 +0700

    Очистка куки

--- lab3.py
+++ lab3.py
@@ -101,7 +101,14 @@ def settings():
                                           font_size=font_size,padding=padding))
     return resp
 
-
+@lab3.route('/lab3/clear_cookies', methods=['POST'])
+def clear_cookies():
+    resp = make_response(redirect(('settings')))
+    resp.delete_cookie('color')
+    resp.delete_cookie('background_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('padding')
+    return resp
 
 @lab3.route('/lab3/ticket', methods=['get', 'post'])
 def ticket():
