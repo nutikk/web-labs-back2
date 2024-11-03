5106746998860a4f7596fb1f860970d7ae7ead59
Анна Петерс <an_peters@mail.ru>
2024-11-03 21:05:09 +0700

    Билет

--- lab3.py
+++ lab3.py
@@ -5,9 +5,10 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
+    age = request.cookies.get('age')
     name = request.cookies.get('name')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 
 @lab3.route('/lab3/cookie')
@@ -36,6 +37,7 @@ def form1():
         errors['user'] = 'Заполните поле!'
     elif age == '':
         errors['age']  = 'Заполнитe поле!'
+   
     return render_template('lab3/form1.html', user=user, sex=sex, age=age, errors=errors)
 
 
@@ -98,3 +100,70 @@ def settings():
                                          background_color=background_color,
                                           font_size=font_size,padding=padding))
     return resp
+
+
+
+@lab3.route('/lab3/ticket', methods=['get', 'post'])
+def ticket():
+    errors = {}
+    if request.method == 'POST':
+        fio = request.form.get('fio')
+        berth = request.form.get('berth')
+        with_linen = request.form.get('with_linen')
+        with_baggage = request.form.get('with_baggage')
+        age = request.form.get('age')
+        departure = request.form.get('departure')
+        destination = request.form.get('destination')
+        travel_date = request.form.get('travel_date')
+        with_insurance = request.form.get('with_insurance')
+
+        if not fio:
+            errors['fio'] = 'Заполните поле ФИО!'
+        if not berth:
+            errors['berth'] = 'Выберите место!'
+        if not age:
+            errors['age'] = 'Заполните поле возраста!'
+        if not departure:
+            errors['departure'] = 'Заполните поле пункта выезда!'
+        if not destination:
+            errors['destination'] = 'Заполните поле пункта назначения!'
+        if not travel_date:
+            errors['travel_date'] = 'Заполните поле даты поездки!'
+
+        if age:
+            age = int(age)
+            if not (1 <= age <= 120):
+                errors['age'] = 'Возраст должен быть от 1 до 120 лет!'
+        else:
+            errors['age'] = 'Заполните поле возраста!'
+
+        if errors:
+            return render_template('lab3/ticket_form.html', errors=errors, fio=fio, berth=berth, with_linen=with_linen, with_baggage=with_baggage, age=age, departure=departure, destination=destination, travel_date=travel_date, with_insurance=with_insurance)
+
+        price = 700 if age < 18 else 1000
+        if berth in ['нижняя', 'нижняя боковая']:
+            price += 100
+        if with_linen:
+            price += 75
+        if with_baggage:
+            price += 250
+        if with_insurance:
+            price += 150
+
+        ticket = {
+            'fio': fio,
+            'berth': berth,
+            'with_linen': 'Да' if with_linen else 'Нет',
+            'with_baggage': 'Да' if with_baggage else 'Нет',
+            'age': age,
+            'departure': departure,
+            'destination': destination,
+            'travel_date': travel_date,
+            'with_insurance': 'Да' if with_insurance else 'Нет',
+            'price': price,
+            'is_child': age < 18
+        }
+
+        return render_template('lab3/ticket.html', ticket=ticket)
+
+    return render_template('lab3/ticket_form.html', errors=errors)
