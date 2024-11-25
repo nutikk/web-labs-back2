169a508dae4f25b6c0e10f8b62b9d8fd99ea68a4
Анна Петерс <an_peters@mail.ru>
2024-11-25 12:20:59 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -160,3 +160,27 @@ def logout():
     return redirect('/lab4/login')
 
 
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    if request.method == 'GET':
+        return render_template('lab4/fridge.html')
+
+    temperature = request.form.get('temperature')
+
+    if not temperature:
+        return render_template('lab4/fridge.html', error='Ошибка: не задана температура')
+
+    temperature = int(temperature)
+
+    if temperature < -12:
+        return render_template('lab4/fridge.html', error='Не удалось установить температуру — слишком низкое значение')
+    elif temperature > -1:
+        return render_template('lab4/fridge.html', error='Не удалось установить температуру — слишком высокое значение')
+    elif -12 <= temperature <= -9:
+        return render_template('lab4/fridge.html', message=f'Установлена температура: {temperature}°С', snowflakes=3)
+    elif -8 <= temperature <= -5:
+        return render_template('lab4/fridge.html', message=f'Установлена температура: {temperature}°С', snowflakes=2)
+    elif -4 <= temperature <= -1:
+        return render_template('lab4/fridge.html', message=f'Установлена температура: {temperature}°С', snowflakes=1)
+
+
