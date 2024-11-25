3127b675544660a060352239df273122a786902f
Анна Петерс <an_peters@mail.ru>
2024-11-25 10:48:12 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -122,6 +122,13 @@ def tree():
     
     return redirect('/lab4/tree')
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'anya', 'password': '777'},
+    {'login': 'john', 'password': '888'}
+]
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -130,7 +137,11 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login']  and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
+        
     error = 'неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
+
+
