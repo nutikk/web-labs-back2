7d57d80f742fcc72620ce1d28d9b9e9e318a7130
Анна Петерс <an_peters@mail.ru>
2024-12-11 17:38:51 +0700

    Хэширование паролей

--- lab5.py
+++ lab5.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, session
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash, generate_password_hash
 
 lab5 = Blueprint('lab5', __name__)
 @lab5.route('/lab5')
@@ -34,13 +35,13 @@ def register():
         return render_template('lab5/register.html', error='Заполните все поля')
     
     conn, cur = db_connect()
-
+    password_hash = generate_password_hash(password)
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error="Такой пользователь уже существует")
     
-    cur.execute (f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');")
+    cur.execute (f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
 
     db_close(conn, cur)
     return render_template('lab5/success.html')
@@ -64,9 +65,10 @@ def login():
         db_close(conn, cur)
         return render_template('lab5/login.html', error="Логин и/или пароль неверны")
     
-    if user['password'] != password:
+    if not check_password_hash(user['password'], password):
         db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+
     
     session['login'] = login
     db_close(conn, cur)
