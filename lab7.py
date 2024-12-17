b4a5b0d82d6b980a3a1342327cd8918040048b19
Анна Петерс <an_peters@mail.ru>
2024-12-17 18:47:10 +0700

    Получение информации о фильме

--- lab7.py
+++ lab7.py
@@ -3,5 +3,41 @@ from flask import Blueprint, render_template, request
 lab7 = Blueprint('lab7', __name__)
 
 @lab7.route('/lab7/')
-def lab():
-    return render_template('lab7/lab7.html')
+def main():
+    return render_template('lab7/index.html')
+
+
+films = [
+    {
+        "title": "The Matrix",
+        "title_ru": "Матрица",
+        "year": 1999,
+        "description": "Хакер Нео узнаёт правду о реальности и своей роли в спасении человечества от машин, которые держат мир в иллюзии."
+    },
+    {
+        "title": "Fight Club",
+        "title_ru": "Бойцовский клуб",
+        "year": 1999,
+        "description": "Анонимный герой создаёт подпольный бойцовский клуб, чтобы снять напряжение от своей унылой жизни, но вскоре всё выходит из-под контроля."
+    },
+    {
+        "title": "Gladiator",
+        "title_ru": "Гладиатор",
+        "year": 2000,
+        "description": "Римский генерал Максимус становится гладиатором и жаждет мести императору, который убил его семью и лишил власти."
+    }
+]
+
+
+@lab7.route('/lab7/rest-api/films/', methods=['GET'])
+def get_films():
+    return films
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
+def get_film(id):
+    if 0 <= id < len(films):
+        return films[id]
+    else:
+        return "Нет фильма под таким индексом", 404
+
