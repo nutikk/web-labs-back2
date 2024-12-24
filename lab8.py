577c0c659cb2d6a1f4e843dd8d738a0d6534f891
Анна Петерс <an_peters@mail.ru>
2024-12-24 16:54:14 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -2,7 +2,7 @@ from flask import Blueprint, url_for, redirect, render_template, request, make_r
 import psycopg2
 from db import db 
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
@@ -12,7 +12,7 @@ lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def lab():
-    return render_template('lab8/lab8.html', login=session.get('login'))
+     return render_template('lab8/lab8.html')
 
 @lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
@@ -39,7 +39,7 @@ def register():
     return redirect('/lab8/')
 
 
-@lab8.route('/lab8/login', methods=['GET', 'POST'])
+@lab8.route('/lab8/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('lab8/login.html')
@@ -68,3 +68,10 @@ def login():
 @login_required
 def article_list():
     return 'Список статей'
+
+@lab8.route('/lab8/logout/')
+@login_required
+def logout():
+    session.pop('login', None)
+    logout_user()
+    return redirect('/lab8/')
